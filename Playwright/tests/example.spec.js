4f4bf45dcc9795213839a0b069d6ae05403d4173
Chirag <bhangalec1609@gmail.com>
2024-08-19 13:01:02 +0530

    mini project

--- Playwright/tests/example.spec.js
+++ Playwright/tests/example.spec.js
@@ -28,7 +28,7 @@ test("mobiles image",async({page})=>{
 })
 
 
-test("title",async({page})=>{
+test("appliance section",async({page})=>{
   await page.goto("https://www.flipkart.com/");
   const appliance = page.getByRole('link',{name: /Appliances/});
   await appliance.nth(0).click();
@@ -38,7 +38,8 @@ test("title",async({page})=>{
 
 test("product heading",async({page})=>{
   await page.goto("https://www.flipkart.com/google-pixel-9-obsidian-256-gb/p/itm330ed8ebeefe1?pid=MOBH2HJGP3FGTECW&lid=LSTMOBH2HJGP3FGTECWIRLKP1&marketplace=FLIPKART&fm=neo%2Fmerchandising&iid=M_fbfa478f-f91c-4561-9f48-d320b05c5f7e_59_VAPFSVIRI72Z_MC.MOBH2HJGP3FGTECW&ppt=clp&ppn=mobile-phones-store&ssid=a4mzohex1c0000001723796709297&otracker=clp_pmu_v2_Google%2Bsmartphones_1_59.productCard.PMU_V2_Google%2BPixel%2B9%2B%2528Obsidian%252C%2B256%2BGB%2529_mobile-phones-store_MOBH2HJGP3FGTECW_neo%2Fmerchandising_0&otracker1=clp_pmu_v2_PINNED_neo%2Fmerchandising_Google%2Bsmartphones_LIST_productCard_cc_1_NA_view-all&cid=MOBH2HJGP3FGTECW");
-  await expect(page.getByRole('heading',{name : 'Google Pixel 9 (Obsidian, 256 GB)'})).toBeVisible();
+  const heading =await page.locator('h1:has-text("Google Pixel 9 (Obsidian, 256 GB)")');
+  await heading.waitFor({ state: 'visible'});
 })
 
 test("add to cart button",async({page})=>{
@@ -85,7 +86,7 @@ test("select offers",async({page})=>{
   await offers.click();
 })
 
-test("show prices",async({page})=>{
+test("show price with discount",async({page})=>{
   await page.goto("https://www.flipkart.com/metronaut-men-solid-casual-dark-green-shirt/p/itm27b1951c2811e?pid=SHTGTCJQGSTR5XDQ&lid=LSTSHTGTCJQGSTR5XDQRZC9FQ&marketplace=FLIPKART&store=clo%2Fash%2Faxc%2Fmmk%2Fkp7&srno=b_1_3&otracker=hp_rich_navigation_3_1.navigationCard.RICH_NAVIGATION_Fashion~Men%2527s%2BTop%2BWear~Men%2527s%2BCasual%2BShirts_S4ITKV5A4VL6&otracker1=hp_rich_navigation_PINNED_neo%2Fmerchandising_NA_NAV_EXPANDABLE_navigationCard_cc_3_L2_view-all&fm=organic&iid=57c765ad-1ff0-456b-903b-9929af19d96b.SHTGTCJQGSTR5XDQ.SEARCH&ppt=browse&ppn=browse&ssid=cneya67ym80000001723804603973");
   await expect(page.locator('div.hl05eU')).toBeVisible();
 })
